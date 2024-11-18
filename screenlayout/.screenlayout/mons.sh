57d4d77a664c8b2eaea682992ccf5c6f57c7437a
Mark Puha <p.mark95@gmail.com>
2024-11-18 09:49:02 +0100

    add screeenlayout

---
+++ screenlayout/.screenlayout/mons.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# case ${MONS_NUMBER} in
+#     1)
+#         mons -o
+#         feh --recursive --bg-fill --randomize ~/wallpapers/
+#         ;;
+#     2)
+        mons -e left
+        feh --recursive --bg-fill --randomize ~/wallpapers/
+        # feh --no-fehbg --bg-fill "${HOME}/wallpapers/a.jpg" \
+        #                --bg-fill "${HOME}/wallpapers/b.jpg"
+    #     ;;
+    # *)
+    #     # Handle it manually
+    #     ;;
+# esac
