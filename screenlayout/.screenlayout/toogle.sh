e0edba6a4cc5f85c6c7e3032588a86a8479de9f8
Mark Puha <p.mark95@gmail.com>
2025-01-01 07:47:14 +0100

    wallpaper change & ghostty integration & faster cursor

--- screenlayout/.screenlayout/toogle.sh
+++ screenlayout/.screenlayout/toogle.sh
@@ -4,7 +4,6 @@ intern=eDP1
 extern1=DP1
 extern2=DP2
 
-# sleep 10
 if xrandr | grep -w "$extern1 connected.*"; then
 	# xrandr --newmode "1280x1024_60.00" 109.00 1280 1368 1496 1712 1024 1027 1034 1063 -hsync +vsync
 	# xrandr --addmode DP1 1280x1024_60.00
@@ -16,4 +15,5 @@ elif xrandr | grep -w "$extern2 connected.*"; then
 else
     xrandr --output "$extern1" --off --output "$extern2" --off --output "$intern" --mode 1920x1080 --pos 0x0
 fi
-feh --recursive --bg-fill --randomize ~/wallpapers/
+# feh --recursive --bg-fill --randomize ~/wallpapers/
+feh --recursive --bg-fill ~/Downloads/macos-monterey-wwdc-21-stock-dark-mode-5k-3840x2160-5585.jpg
